8b20a07714d8c5487992aaab7cb562bd56b1c236
givaralv <givaralv@gmail.com>
2025-08-25 15:20:26 -0500

    Conectar a BD Postgres

--- funerals/settings.py
+++ funerals/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'funerals.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'DBFunerales',
+		'USER':'postgres',
+		'PASSWORD':'adminn',
+		'HOST':'127.0.0.1',
+		'DATABASE_PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
